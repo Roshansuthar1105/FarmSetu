34b1e043f08a42905c8fc82c8e02899eaedc879f
Roshansuthar2023@gmail.com <roshansuthar2023@gmail.com>
2025-03-30 17:01:52 +0530

    news component change

--- frontend/src/pages/NewsFeed.jsx
+++ frontend/src/pages/NewsFeed.jsx
@@ -16,44 +16,44 @@ const NewsFeed = () => {
         } finally {
           setLoading(false);
         }
-      //   // const url = 'https://google-news13.p.rapidapi.com/search?keyword=agriculture&lr=en-US';
-      //   const url =`https://newsapi.org/v2/top-headlines?q=agriculture&from=2024-11-04&to=${new Date().toISOString().split('T')[0]}&sortBy=relevancy&apiKey=3bd19f3433c440e68ecaed975c5e2d4d`;
-      //   console.log(url);
-      //   const options = {
-      //     method: 'GET',
-      //   };
+        // const url = 'https://google-news13.p.rapidapi.com/search?keyword=agriculture&lr=en-US';
+        // const url =`https://newsapi.org/v2/top-headlines?q=agriculture&from=2024-11-04&to=${new Date().toISOString().split('T')[0]}&sortBy=relevancy&apiKey=3bd19f3433c440e68ecaed975c5e2d4d`;
+        // console.log(url);
+        // const options = {
+        //   method: 'GET',
+        // };
 
-      //   try {
-      //     const response = await fetch(url, options);
-      //     const result = await response.json();
-      //     console.log(result.articles[0]);
-      //     setNewsData(result.articles);
-      //   } catch (error) {
-      //     console.error(error);
-      //   } finally {
-      //     setLoading(false);
-      //   }
+        // try {
+        //   const response = await fetch(url, options);
+        //   const result = await response.json();
+        //   console.log(result.articles[0]);
+        //   setNewsData(result.articles);
+        // } catch (error) {
+        //   console.error(error);
+        // } finally {
+        //   setLoading(false);
+        // }
 
-      // const url = 'https://real-time-news-data.p.rapidapi.com/search?query=agriculture&time_published=anytime&country=US&lang=en';
-      // const options = {
-      //   method: 'GET',
-      //   headers: {
-      //     'x-rapidapi-key': '2cbdd8bf5cmsh714c8b4ba144e97p104dddjsn2493098c2349',
-      //     'x-rapidapi-host': 'real-time-news-data.p.rapidapi.com'
-      //   }
-      // };
+    //   const url = 'https://real-time-news-data.p.rapidapi.com/search?query=agriculture&time_published=anytime&country=US&lang=en';
+    //   const options = {
+    //     method: 'GET',
+    //     headers: {
+    //       'x-rapidapi-key': '2cbdd8bf5cmsh714c8b4ba144e97p104dddjsn2493098c2349',
+    //       'x-rapidapi-host': 'real-time-news-data.p.rapidapi.com'
+    //     }
+    //   };
 
-      // try {
-      //   const response = await fetch(url, options);
-      //   const result = await response.json();
-      //   setNewsData(result.data);
-      //   console.log(result.data);
-      // } catch (error) {
-      //   console.error(error);
-      // }
-      // finally {
-      //   setLoading(false);
-      // }
+    //   try {
+    //     const response = await fetch(url, options);
+    //     const result = await response.json();
+    //     setNewsData(result.data);
+    //     console.log(result.data);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    //   finally {
+    //     setLoading(false);
+    //   }
     };
 
 
@@ -65,7 +65,7 @@ const NewsFeed = () => {
   }
   return (
     <div className="bg-gray-900 min-h-screen flex flex-col">
-      <div className="pt-20 max-w-3xl mx-auto my-8 px-4">
+      <div className="pt-20 max-w-3xl mx-auto my-8 px-4 md:px-0">
         <h1 className="text-4xl font-bold mb-6 text-center text-green-500 transition duration-500 ease-in-out transform hover:scale-105">
           Agriculture News Feed
         </h1>
@@ -77,13 +77,13 @@ const NewsFeed = () => {
         ) : error ? (
           <p className="text-red-500 text-center">{error}</p>
         ) : (
-          <div className="space-y-8"> {/* Increased spacing between cards */}
+          <div className="space-y-8 px-4 sm:px-10 "> {/* Increased spacing between cards */}
             {newsData && newsData.slice(limit-10, limit).map((item, index) => (
               <div
                 key={index}
-                className="bg-green-800 shadow-lg rounded-lg overflow-hidden flex flex-col md:flex-row transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl"
+                className="bg-green-800 shadow-lg rounded-lg overflow-hidden flex flex-col md:flex-row lg:flex-row transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl"
               >
-                <div className="flex-grow w-2/5">
+                <div className="flex-grow w-full md:w-1/2 lg:w-2/5">
                   {(
                     <img
                       src={item.photo_url}
@@ -92,7 +92,7 @@ const NewsFeed = () => {
                     />
                   )}
                 </div>
-                <div className="flex-grow p-6 w-3/5">
+                <div className="flex-grow p-6 w-full md:w-1/2 lg:w-3/5">
                   <a
                     href={item.link}
                     target="_blank"
@@ -115,7 +115,7 @@ const NewsFeed = () => {
                 </div>
               </div>
             ))}
-            <div className="flex justify-between sticky bottom-0 p-1 backdrop-blur-sm bg-[#ffffff29] border-t-3 border-gray-900 ">
+            <div className="flex justify-between sticky bottom-0 p-1 backdrop-blur-sm bg-[#ffffff29] border-t-3 border-gray-900 md:px-4">
               <button className="bg-green-500 text-white px-4 py-2 rounded-md" disabled={limit === 10} onClick={()=>loadMore(-10)} >Prev</button>
               <button className="bg-green-500 text-white px-4 py-2 rounded-md" disabled={limit >= newsData.length} onClick={()=>loadMore(10)} >Next</button>
             </div>
